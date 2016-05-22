f10ff1f28244514156800738cdc6cd714042a9ed
Femaref <femaref@googlemail.com>
2016-05-22 14:00:18 +0200

    gofmt

--- cmd/proxy/proxy.go
+++ cmd/proxy/proxy.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
-    "os"
-    "net/url"
-    "encoding/json"
-    "xmljsonproxy"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"xmljsonproxy"
 )
 
 func writeJson(w http.ResponseWriter, obj interface{}, code int) error {
-	marshalled, err := json.MarshalIndent(map[string]interface{}{"result":obj}, "", "  ")
+	marshalled, err := json.MarshalIndent(map[string]interface{}{"result": obj}, "", "  ")
 	if err != nil {
 		return err
 	}
@@ -26,48 +26,45 @@ var ccpEndpoint, _ = url.Parse("https://api.eveonline.com")
 type server struct {
 }
 
-
 func makeRequest(w http.ResponseWriter, r *http.Request) {
-    targetUrl := ccpEndpoint.ResolveReference(r.URL)
-    resp, err := http.Get(targetUrl.String())
-    
-    if err != nil {
-        fmt.Println(err)
-        writeJson(w, err.Error(), 400)
-        return
-    }
-    
-    result, err := xmljsonproxy.Transform(resp.Body)
-    
-    if err != nil {
-        fmt.Println(err)
-        writeJson(w, err.Error(), 400)
-        return
-    }
-    w.Header().Add("Content-Type", "application/json")
-    w.WriteHeader(resp.StatusCode)
-    w.Write(result)
-    
-    return    
+	targetUrl := ccpEndpoint.ResolveReference(r.URL)
+	resp, err := http.Get(targetUrl.String())
+
+	if err != nil {
+		fmt.Println(err)
+		writeJson(w, err.Error(), 400)
+		return
+	}
+
+	result, err := xmljsonproxy.Transform(resp.Body)
+
+	if err != nil {
+		fmt.Println(err)
+		writeJson(w, err.Error(), 400)
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(resp.StatusCode)
+	w.Write(result)
+
+	return
 }
 
+func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	makeRequest(w, r)
 
-func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {    
-    makeRequest(w, r)
-    
-    return
+	return
 }
 
 func main() {
-    r := server{}
-    
-    
-    if ccpendpoint := os.Getenv("CCPENDPOINT"); ccpendpoint != "" {
-        ccpEndpoint, _ = url.Parse(ccpendpoint)
-    }
+	r := server{}
+
+	if ccpendpoint := os.Getenv("CCPENDPOINT"); ccpendpoint != "" {
+		ccpEndpoint, _ = url.Parse(ccpendpoint)
+	}
 	if port := os.Getenv("PORT"); port != "" {
-	    http.ListenAndServe(fmt.Sprintf(":%s", port), r)
+		http.ListenAndServe(fmt.Sprintf(":%s", port), r)
 	} else {
-	    http.ListenAndServe(":9293", r)
+		http.ListenAndServe(":9293", r)
 	}
 }
--- transform.go
+++ transform.go
@@ -1,166 +1,163 @@
 package xmljsonproxy
 
 import (
-    "encoding/json"
-    "encoding/xml"
-    "strconv"
-    "io"
-    "strings"
-    "bytes"
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"io"
+	"strconv"
+	"strings"
 )
 
-func coerce(s string) (interface{}) {
-    var err error
-    
-    i, err := strconv.ParseInt(s, 10, 64)
-    
-    if err == nil {
-        return i
-    }
-    
-    fl, err := strconv.ParseFloat(s, 64)
-    
-    if err == nil {
-        return fl
-    }
-    
+func coerce(s string) interface{} {
+	var err error
 
-    
-    b, err := strconv.ParseBool(s)
-    
-    if err == nil {
-        return b
-    }
-    
-    return s
+	i, err := strconv.ParseInt(s, 10, 64)
+
+	if err == nil {
+		return i
+	}
+
+	fl, err := strconv.ParseFloat(s, 64)
+
+	if err == nil {
+		return fl
+	}
+
+	b, err := strconv.ParseBool(s)
+
+	if err == nil {
+		return b
+	}
+
+	return s
 }
 
 func transformObject(d *xml.Decoder, st xml.StartElement) (interface{}, error) {
-    var mapOutput map[string]interface{} = make(map[string]interface{})
-    var token xml.Token
-    
-    for _, e := range st.Attr {
-        mapOutput[e.Name.Local] = coerce(e.Value)
-    }
+	var mapOutput map[string]interface{} = make(map[string]interface{})
+	var token xml.Token
 
-    var stringOutput string
-    
-Loop:    
-    for true {
-        token, _ = d.Token()
-        
-        switch token := token.(type) {
-            case xml.StartElement:
-                if token.Name.Local == "rowset" {
-                    name, rowset, err := transformRowset(d, token)
-                    if err != nil {
-                        return nil, err
-                    }
-                    mapOutput[name] = rowset
-                } else {
-                    field, err := transformObject(d, token)
-                    if err != nil {
-                        return nil, err
-                    }
-                    mapOutput[token.Name.Local] = field
-                }
-                break
-            case xml.EndElement:
-                if token.Name == st.Name {
-                    break Loop
-                }
-            case xml.CharData:
-                r := string(token)
-                if strings.TrimSpace(r) != "" {
-                    stringOutput = r
-                }
-                
-        }
-    }
-    
-    var output interface{}
-    
-    if stringOutput != "" {
-        output = stringOutput
-    } else {
-        output = mapOutput
-    }
-    
-    return output, nil
+	for _, e := range st.Attr {
+		mapOutput[e.Name.Local] = coerce(e.Value)
+	}
+
+	var stringOutput string
+
+Loop:
+	for true {
+		token, _ = d.Token()
+
+		switch token := token.(type) {
+		case xml.StartElement:
+			if token.Name.Local == "rowset" {
+				name, rowset, err := transformRowset(d, token)
+				if err != nil {
+					return nil, err
+				}
+				mapOutput[name] = rowset
+			} else {
+				field, err := transformObject(d, token)
+				if err != nil {
+					return nil, err
+				}
+				mapOutput[token.Name.Local] = field
+			}
+			break
+		case xml.EndElement:
+			if token.Name == st.Name {
+				break Loop
+			}
+		case xml.CharData:
+			r := string(token)
+			if strings.TrimSpace(r) != "" {
+				stringOutput = r
+			}
+
+		}
+	}
+
+	var output interface{}
+
+	if stringOutput != "" {
+		output = stringOutput
+	} else {
+		output = mapOutput
+	}
+
+	return output, nil
 }
 
 func transformRowset(d *xml.Decoder, st xml.StartElement) (string, []interface{}, error) {
-    var output []interface{}
-    var token xml.Token
+	var output []interface{}
+	var token xml.Token
 
+	var name string
+	for _, e := range st.Attr {
+		if e.Name.Local == "name" {
+			name = e.Value
+			break
+		}
+	}
 
-    var name string
-    for _, e := range st.Attr {
-        if e.Name.Local == "name" {
-            name = e.Value
-            break
-        }
-    }
+Loop:
+	for true {
+		token, _ = d.Token()
 
-Loop:    
-    for true {
-        token, _ = d.Token()
-        
-        switch token := token.(type) {
-            case xml.StartElement:
-                row, err := transformObject(d, token)
-                if err != nil {
-                    return name, nil, err
-                }
-                output = append(output, row)
-            case xml.EndElement:
-                if token.Name.Local == "rowset" {
-                    break Loop
-                }
-        }
-    }
-    
-    return name, output, nil
+		switch token := token.(type) {
+		case xml.StartElement:
+			row, err := transformObject(d, token)
+			if err != nil {
+				return name, nil, err
+			}
+			output = append(output, row)
+		case xml.EndElement:
+			if token.Name.Local == "rowset" {
+				break Loop
+			}
+		}
+	}
+
+	return name, output, nil
 }
 
 func Transform(body io.Reader) ([]byte, error) {
-    d := xml.NewDecoder(body)
-    var token xml.Token
-    var err error
-    
-    var output map[string]interface{} = make(map[string]interface{})
-    
-    for true {
-        token, err = d.Token()
-        if err == io.EOF {
-            break
-        }
-        
-        switch token := token.(type) {
-            case xml.StartElement:
-                switch token.Name.Local {
-                    case "eveapi":
-                        for _, e := range token.Attr {
-                            output[e.Name.Local] = coerce(e.Value)
-                        }
-                    default:
-                        field, err := transformObject(d, token)
-                        
-                        if err != nil {
-                            return nil, err
-                        }
-                        output[token.Name.Local] = field
-                }
-        }
-        
-    }
-    
-    return json.Marshal(output)
+	d := xml.NewDecoder(body)
+	var token xml.Token
+	var err error
+
+	var output map[string]interface{} = make(map[string]interface{})
+
+	for true {
+		token, err = d.Token()
+		if err == io.EOF {
+			break
+		}
+
+		switch token := token.(type) {
+		case xml.StartElement:
+			switch token.Name.Local {
+			case "eveapi":
+				for _, e := range token.Attr {
+					output[e.Name.Local] = coerce(e.Value)
+				}
+			default:
+				field, err := transformObject(d, token)
+
+				if err != nil {
+					return nil, err
+				}
+				output[token.Name.Local] = field
+			}
+		}
+
+	}
+
+	return json.Marshal(output)
 }
 
 func TransformString(s string) (string, error) {
-    buffer := bytes.NewBufferString(s)
-    b, err := Transform(buffer)
-    
-    return string(b), err
+	buffer := bytes.NewBufferString(s)
+	b, err := Transform(buffer)
+
+	return string(b), err
 }
